6e39904b6b3e3e99887f5077f077c3a30b558e7d
cosmicoboy <nahuel_venus@outlook.com>
2021-07-15 19:49:40 -0300

    second  commit

--- src/Components/helpers/heroImages.js
+++ src/Components/helpers/heroImages.js
@@ -2,5 +2,17 @@
 //Para exportar imagenes correctamente y sin problemas
 
 // export const godsImages = require.context('/../assets/gods', true)
-export const godsImages = require.context('/../assets/gods',  false, /\.(png|jpe?g|svg)$/)
+deityImages = require.context('/../assets/gods',  false  , /\.(png|jpe?g|svg)$/)
+
+const importAll = (r) => {
+    return r.keys().map(r);
+}
+
+console.log(deityImages);
+
+export const godsImages =importAll(deityImages)
+
+
+
+
 
